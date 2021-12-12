f741f5a19f13dbcf58a412586f41f6b038858b54
Turk00111 <zack.throw@gmail.com>
2021-12-12 09:43:00 -0800

    Add randNum, msg1, msg2 generator

---
+++ mixedMsgs.js
@@ -0,0 +1,53 @@
+//sript is to test to make sure node is working
+//console.log('script is working')
+
+const randNum = (maxNum) => {
+    if (maxNum === undefined) {
+        maxNum = 0
+    }
+    return Math.floor(Math.random() * maxNum)
+    
+};
+
+
+const msg1 = () => { 
+    let msgNum = randNum(2);
+    switch(msgNum) { 
+        case '0':
+            return 'Knock Knock';
+            break;
+        case '1':
+            return 'Not in the mood for a joke. Try again later!';
+            break;
+        default:
+            return 'There was an error in generating Message 1'; 
+            break; 
+    };
+};
+
+const msg2 =  () => {
+    let msgNum = randNum(4);
+    switch(msgNum) { 
+        case '0': 
+            return 'Orange'; 
+            break;
+        case '1': 
+            return 'Who';
+            break;
+        case '2': 
+            return 'banana';
+            break;
+        case '3': 
+            return 'brick';
+            break;
+        default: 
+            return 'There was an error in generating Message 2';
+            break;
+    }
+};
+
+const tempMsg1 = msg1(); 
+const tempMsg2 = msg2(); 
+
+
+
